4b57dec7c006306871155f037edd0b74b1e984dd
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-05-01 09:29:28 -0400

    param parser?

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,52 @@
-use std::io;
+use std::fs;
 use std::env;
+use std::io::Error;
+use std::path::{PathBuf};
+
 
 fn main() {
-    println!("Guess the number!");
 
-    println!("Please input your guess.");
+    for arg in env::args().skip(1) {
+        let path_raw = PathBuf::from(arg);
+        let real_path = path_raw.canonicalize().expect("Invalid Path");
+        let file_metadata = fs::metadata(&real_path).expect("Error with Metadata");
+    }
+}
 
-    let mut guess = String::new();
+fn is_param(input: String) -> bool {
+    return input.starts_with("-");
+}
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+struct Args {
+    recurse: bool,
+    verbose: bool,
+    interactive: bool,
+    force: bool
+}
 
-    println!("You guessed: {guess}");
+fn parse_params(params: String) -> Result<Args,()> {
+    if !params.starts_with("-") {
+        Err::<Args, ()>(());
+    }
+    let cleanParams = params.split('-').nth(1);
+    println!("Clean Params {:#?}", cleanParams.unwrap());
+    let parsed = Args {
+        recurse: false,
+        verbose: false,
+        interactive: false,
+        force: false
+    };
+    // for singleparam in cleanParams {
+    //     if(singleparam == "r") {
+    //         parsed.recurse == true;
+    //     } else if (singleparam == "v") {
+    //         parsed.verbose == true;
+    //     } else if (singleparam == "i") {
+    //         parsed.interactive == true;
+    //     } else if (singleparam == "f") {
+    //         parsed.interactive == true;
+    //     }
+    // }
+
+    Ok(parsed)
 }
